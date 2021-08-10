e46a466ad9b20e940d33e4e4420a4116f881b9d8
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-10 19:38:19 -0400

    10. Usando parciales con HBS

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const hbs = require("hbs");
 const express = require("express");
-const app = express();
 
+const app = express();
 const port = 8080;
 
+// ? handleBars
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 // ? servir contenido estatico
 app.use(express.static("public"));
@@ -14,12 +17,22 @@ app.get("/", (req, res) => {
       title: "Curso de node",
    });
 });
+
 app.get("/elements", (req, res) => {
-   res.sendFile(__dirname + "/public/elements.html");
+   res.render("partials/elements", {
+      name: "Gerardo Miranda",
+      title: "Curso de node",
+   });
+
+   // res.sendFile(__dirname + "/public/elements.html");
 });
 
 app.get("/generic", (req, res) => {
-   res.sendFile(__dirname + "/public/generic.html");
+   res.render("partials/generic", {
+      name: "Gerardo Miranda",
+      title: "Curso de node",
+   });
+   // res.sendFile(__dirname + "/public/generic.html");
 });
 
 app.get("*", (req, res) => {
