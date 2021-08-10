f89db1c0d52f972098b39ef14079bf7b6d0d583c
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-10 18:47:18 -0400

    7. Servir un sitio web completo

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ const port = 8080;
 // ? servir contenido estatico
 app.use(express.static("public"));
 
-app.get("/hola-mundo", (req, res) => {
-   res.send("hola mundo es otra ruta");
+// app.get("/hola-mundo", (req, res) => {
+//    res.send("hola mundo es otra ruta");
+// });
+
+app.get("/elements", (req, res) => {
+   res.sendFile(__dirname + "/public/elements.html");
+});
+
+app.get("/generic", (req, res) => {
+   res.sendFile(__dirname + "/public/generic.html");
 });
 
 app.get("*", (req, res) => {
-   // res.send("404 page not found");
    res.sendFile(__dirname + "/public/404.html");
-   console.log(__dirname);
 });
 
 app.listen(port, () => {
