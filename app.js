0c0eb42a77c75c83f3ad7617cfb7b42db7089301
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-09 20:53:20 -0400

    4. Request y Response

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ const http = require("http");
 
 http
    .createServer((req, res) => {
-      res.write("hola mundo");
+      //   res.writeHead(200, { "Content-Type": "application/json" });
+      res.setHeader(
+         "Content-disposition",
+         "attachment; filename=lista.csv"
+      );
+      res.writeHead(200, { "Content-Type": "application/csv" });
+
+      res.write("id, nombre\n");
+      res.write("1, Gerardo\n");
+      res.write("2, Maria\n");
+      res.write("3, juan\n");
+      res.write("4, pedro\n");
       res.end();
    })
    .listen(8080);
